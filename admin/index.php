bb3bfbf298f35fde0f089de9dd025fbc26f10156
Aung Minn Khant <aungminnkhant94@gmail.com>
2021-01-04 23:49:51 +0630

    pagination and search

--- admin/index.php
+++ admin/index.php
@@ -18,10 +18,40 @@ if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
               <div class="card-header">
                 <h3 class="card-title">Blog Listings</h3>
               </div>
-              <?php
-                $statement = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
-                $statement->execute();
-                $result = $statement->fetchAll(); 
+              <?php 
+              //pagination
+                if(!empty($_GET['pageno'])) {
+                  $pageno = $_GET['pageno'];
+                }else{
+                  $pageno = 1;
+                }
+                $numOfRecs = 1;
+                $offset = ($pageno - 1) * $numOfRecs ;
+              //pagination
+
+                if (empty($_POST['search'])) {
+                  $statement = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC");
+                  $statement->execute();
+                  $rawresult = $statement->fetchAll(); 
+
+                  $total_pages = ceil(count($rawresult)/$numOfRecs);
+
+                  $stmt = $pdo->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT $offset,$numOfRecs");
+                  $stmt->execute();
+                  $result = $stmt->fetchAll();
+                }else{
+                  $searchKey = $_POST['search'];
+                  
+                  $statement = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%searchKey%' ORDER BY id DESC");
+                  $statement->execute();
+                  $rawresult = $statement->fetchAll(); 
+
+                  $total_pages = ceil(count($rawresult)/$numOfRecs);
+
+                  $stmt = $pdo->prepare("SELECT * FROM posts WHERE title LIKE '%searchKey%' ORDER BY id DESC LIMIT $offset,$numOfRecs");
+                  $stmt->execute();
+                  $result = $stmt->fetchAll();
+                }
               ?>
               <!-- /.card-header -->
               <div class="card-body">
@@ -49,7 +79,9 @@ if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
                       </td>
                       <td>
                         <a class="btn btn-warning"href="edit.php?id=<?php echo $value['id']; ?>">Edit</a>
-                        <a href="delete.php?id=<?php echo $value['id'] ?>"onclick="return confirm('Are you sure you want o delete this item')" class="btn btn-danger">Delete</a>
+                        <a href="delete.php?id=<?php echo $value['id'] ?>"
+                           onclick="return confirm('Are you sure you want o delete this item')" 
+                           class="btn btn-danger">Delete</a>
                       </td>
                     </tr>
                     <?php
@@ -59,6 +91,27 @@ if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
                     ?>
                   </tbody>
                 </table>
+                <!--pagination -->
+                <ul class="pagination mt-2"style="float:right;">
+                      <li class="page-item">
+                        <a href="?pageno=1" class="page-link">First</a>
+                      </li>
+                      <li class="page-item <?php if($pageno <= 1){echo 'disabled';} ?> ">
+                        <a href="<?php if($pageno <= 1){echo '#';} else {echo "?pageno=".($pageno - 1);} ?>" 
+                           class="page-link">Previous</a>
+                      </li>  
+                      <li class="page-item">
+                        <a href="" class="page-link"><?php echo $pageno; ?></a>
+                      </li>
+                      <li class="page-item <?php if($pageno >= $total_pages){echo 'disabled';} ?>">
+                        <a href="<?php if($page >= $total_pages) {echo '#';} else {echo"?pageno=".($pageno + 1);} ?>" 
+                           class="page-link">Next</a>
+                      </li>
+                      <li class="page-item">
+                        <a href="?pageno=<?php echo $total_pages ?>" class="page-link">Last</a>
+                      </li>                 
+                </ul>
+                <!-- /.pagination -->
               </div>
               <!-- /.card-body -->
 
